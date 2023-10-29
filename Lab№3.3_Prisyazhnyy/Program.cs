577fd001513c7e33dafccac3f1805181ae6e967d
prisy <prisy@LAPTOP-23R98U4U>
2023-10-29 23:35:22 +0400

    Добавьте файлы проекта.

---
+++ Lab№3.3_Prisyazhnyy/Program.cs
@@ -0,0 +1,50 @@
+﻿//сборник задач/тема 3/подтема 3.3/вариант 22
+
+Console.Write("Введите число x: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите количество повторений n: ");
+double n = double.Parse(Console.ReadLine());
+double numerator = 3;               //‾‾|
+double moveNumerator = 0;           //  |--->  переменные взаимодействующие между собой
+double denominatorAndDegree = 2;    //__|
+double sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    double factorial = 1;
+    for(int j = 1; j <= denominatorAndDegree; j++) factorial *= j;
+    if(i % 2 == 1 && i > 1)
+    {
+        sum -= (numerator / factorial) * Math.Pow(x, denominatorAndDegree);
+    } else
+    {
+        sum += (numerator / factorial) * Math.Pow(x, denominatorAndDegree);
+    }
+    if (i == 1) sum = 1 - sum;
+        numerator += moveNumerator + denominatorAndDegree;
+        moveNumerator += 3;
+        denominatorAndDegree += 2;
+}
+Console.WriteLine($"Ответ: {sum:F2}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for(int i = 0; i <= n; i++)
+//{
+//    for (int j = 1; j <= f; j++)
+//    {
+//        factorial *= j;
+//    }
+    
+//}
+//sum = (3/sum)*Math.Pow(x, degree);
+//Console.WriteLine(1-sum);
